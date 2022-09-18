91c8ac659d6ef715662f62504f0bb565e335e8e8
Daniel Aguirre <daguirre@nexient.com>
2022-09-18 01:46:35 -0500

    removed unused method

--- RedRainParks.Domain/Extensions/TypeExtensions.cs
+++ RedRainParks.Domain/Extensions/TypeExtensions.cs
@@ -4,13 +4,6 @@ namespace RedRainParks.Domain.Extensions
 {
     public static class TypeExtensions
     {
-        public static string GetFetchableColumnNamesForSelect(this Type? type) => type?.GetProperties()
-            .Where(c => c.CustomAttributes.Any(a => a.AttributeType == typeof(FetchableAttribute)))?
-                .Select(p => ((Attribute.GetCustomAttribute(p, typeof(FetchableAttribute)) as FetchableAttribute)!
-                    .SpecifiedColumnName ?? p.Name) + $" AS {p.Name}")
-                        .AggregateWithCommas() ?? string.Empty;
-
-
         public static (string SpecifiedOrDefaultName, string ActualPropertyName)[] GetSqlPropertyNames(this Type? type) => type?.GetProperties()
             .Where(c => c.CustomAttributes.Any(a => a.AttributeType.IsSubclassOf(typeof(SqlPropertyIdentiferAttribute))))?
                 .Select(p => ((Attribute.GetCustomAttribute(p, typeof(SqlPropertyIdentiferAttribute)) as SqlPropertyIdentiferAttribute)!
